587818aef6ffe967bd902eec68718fc269b45c8d
Mac Chapman <mac@veryhappythings.co.uk>
2015-05-12 08:49:18 +0100

    Full calculations for sugar types

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,7 +21,13 @@ brewngControllers.controller('PrimingController', ['$scope', function ($scope) {
     return (3.0378 - (0.050062 * temperature_f) + (0.00026555 * Math.pow(temperature_f, 2))).toFixed(2);
   };
 
-  $scope.quantity = "20";
+  $scope.pureGlucose = function () {
+    // http://byo.com/issues/item/1271-priming-with-sugar
+    // (X g glucose) = Y volumes of CO2 / (88 g CO2/180 g glucose) x (1 mole/44 g CO2) x (22.4 L/mole) x (1/18.9 L beer)
+    return (($scope.co2 - $scope.currentVolume()) / ((88.0 / 180.0) * (1.0 / 44.0) * 22.4 * (1.0 / $scope.quantity))).toFixed(2);
+  };
+
+  $scope.quantity = "19";
   $scope.co2 = "2.0";
   $scope.temperature_c = "20";
 }]);
